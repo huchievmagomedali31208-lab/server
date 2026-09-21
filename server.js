427668d585139cbd7426a3c0a17748571e92df11
Хучиев Магомед-Али К <student367@ithub.local>
2026-09-21 13:49:47 +0300

    server

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const http = require("http")
+const express = require("express")// подключаем
 
-const server = http.createServer((req, res)=>{
-    res.end("Hello World")
+const app = express()
+const Port = 3000;
 
+app.get('/' , (req , res) => {
+    
 })
+
+// const server = http.createServer((req, res)=>{
+//     res.end("Hello World")
+
+// })
 
 server.listen(3000 , ()=>{
     console.log("Сервер запущен успешно по адресу: 127.0.0.1:3000np,");
